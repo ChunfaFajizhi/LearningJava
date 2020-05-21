ad82a97ef1d8bc90eda84075445624386d2e527c
Zhang, Chunfa <34845575+ChunfaFajizhi@users.noreply.github.com>
2020-05-20 22:33:46 -0400

    Update URLReader.java

--- URLReader.java
+++ URLReader.java
@@ -5,13 +5,13 @@ import java.nio.charset.Charset;
 public class URLReader {
     public static void main(String[] args) throws Exception {
 
-        URL fajizhiCA = new URL("https://www.fajizhi.ca");
+        URL fajizhiCA = new URL("https://www.***.ca");
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(fajizhiCA.openStream()));
 
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
-            if (inputLine.contains("img")) {
+            if (inputLine.contains("info")) {
             System.out.println(inputLine);};
         }
         in.close();
